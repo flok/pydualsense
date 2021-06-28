24c628a182d950838e246cf63beb15fcaca1653f
Florian Kaiser <37000563+flok@users.noreply.github.com>
2021-06-28 23:04:55 +0200

    Update package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pydualsense',
-    version='0.5.3',
+    version='0.5.5',
     description='use your DualSense (PS5) controller with python',
     long_description=long_description,
     long_description_content_type="text/markdown",
