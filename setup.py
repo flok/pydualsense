7c79dc5fbf6df64c123ca56bed5932c46a79f135
Flo <37000563+flok@users.noreply.github.com>
2023-01-04 17:57:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pydualsense',
-    version='0.6.2',
+    version='0.6.3',
     description='use your DualSense (PS5) controller with python',
     long_description=long_description,
     long_description_content_type="text/markdown",
